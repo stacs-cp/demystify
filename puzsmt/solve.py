b5fa11d21d442b3b53560ca8ddfea4ad4497169b
Chris Jefferson <caj21@st-andrews.ac.uk>
2020-08-19 11:22:39 +0100

    Make nicer HTML output

--- puzsmt/solve.py
+++ puzsmt/solve.py
@@ -3,9 +3,56 @@ import sys
 
 from .prettyprint import print_explanation
 
+# Make a unique id
+id_counter = 0
+def get_id():
+    global id_counter
+    id_counter += 1
+    return "id{}".format(id_counter)
+
+# Make a div which starts hidden
+def hidden(name, content):
+    id = get_id()
+    s = ""
+    s += """<input type='submit' value='{}' onclick="toggle('{}'):>""".format(name, id)
+    s += "<div id={} style='display:none;'>\n".format(id)
+    s += content
+    s += "\n</div>\n"
+    return s
+
+
+def explain(solver, lit, reason):
+    exp = ""
+    exp += "<p>Setting " + str(lit) + " because:</p>\n"
+    exp += "<ul>\n"
+    for clause in sorted(reason):
+        exp += "<li>" + str(solver.explain(clause)) + "</li>\n"
+    exp += "</ul>\n"
+
+    return exp
 
 def html_solve(outstream, solver, puzlits, MUS):
     trace = []
+
+    # Set up Javascript
+    print("""
+    <script>
+    toggle = function(id) {
+        div = document.getElementById(id)
+        if( div.style.display == "none" ) {
+           div.style.display = "block";
+        } else {
+           div.style.display = "none";
+        }
+    };
+
+    hide = function(id) {
+        div = document.getElementById(id)
+        div.style.display = "none";
+    };
+    </script>
+    """)
+
     # Now, we need to check each one in turn to see which is 'cheapest'
     while len(puzlits) > 0:
         musdict = MUS.smallestMUS(puzlits)
@@ -19,6 +66,9 @@ def html_solve(outstream, solver, puzlits, MUS):
 
             print("Doing", len(lits), " simple deductions ")
 
+            exps = "\n".join([explain(solver, p, musdict[p]) for p in sorted(lits)])
+            print(hidden("Show why", exps))
+
             for p in lits:
                 solver.addLit(p)
                 puzlits.remove(p)
@@ -29,12 +79,12 @@ def html_solve(outstream, solver, puzlits, MUS):
             print_explanation(outstream, solver, musdict[p], [p])
             print("Smallest mus size:", smallest)
             trace.append((smallest, mins))
-            print("<p>Setting ", p," because:</p>")
-            print("<ul>")
-            for clause in sorted(musdict[p]):
-                logging.info(clause.clauseset())
-                print("<li>", solver.explain(clause), "</li>")
-            print("</ul>")
+            print(explain(solver, p, musdict[p]))
+            if len(mins) > 1:
+                print(hidden("There were {} choices of the same size".format(len(mins)-1),
+                    "\n".join([explain(solver, p, musdict[p]) for p in mins[1:]])))
+            else:
+                print("<p>No other choices</p>")
             solver.addLit(p)
             puzlits.remove(p)
         
