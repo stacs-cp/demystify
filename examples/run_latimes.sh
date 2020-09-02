482f889519a98e59da655046757991063288e4a3
Chris Jefferson <caj21@st-andrews.ac.uk>
2020-09-02 22:54:46 +0100

    Prepare latimes

--- examples/run_latimes.sh
+++ examples/run_latimes.sh
@@ -2,19 +2,21 @@
 mkdir latimes-results
 
 for i in 3 2 1 0; do
-    for solver in 
-            '{}'
-            '{"prechopMUSes12": True }'
-            '{"prechopMUSes12": True, "baseSizeMUS": 10000 }'
-            '{"prechopMUSes12": True, "useUnsatCores": False }'
-            '{"tryManyChopMUS": True}'
-            '{"minPrecheckMUS": True}'
-            '{"gallopingMUSes": True}'
-            '{"gallopingMUSes": True, "baseSizeMUS": 10000}'
-            '{"minPrecheckStepsMUS": True}'
-            '{"gallopingMUSes": True, "minPrecheckMUS": True}'
+    for solver in  \
+            '{}' \
+            '{"baseSizeMUS": 100000}' \
+            '{"useUnsatCores": false}' \
+            '{"baseSizeMUS": 100000, "useUnsatCores": false}' \
+            '{"prechopMUSes12": true }' \
+            '{"prechopMUSes12": true, "useUnsatCores": false }' \
+            '{"prechopMUSes12": true, "useUnsatCores": false, "baseSizeMUS": 100000 }' \
+            '{"tryManyChopMUS": true}' \
+            '{"gallopingMUSes": true}' \
+            '{"gallopingMUSes": true, "baseSizeMUS": 100000}' \
+            '{"minPrecheckStepsMUS": true}' \
+            '{"minPrecheckMUS": true}' 
     
-    ; do
+    do
         (time ./latimes.py $i "$solver") > "latimes-results/$i-$solver.html" 2> "latimes-results/$i-$solver.log" &
     done
     wait
