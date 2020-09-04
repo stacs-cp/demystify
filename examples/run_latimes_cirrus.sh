5ab7d5f5aef663e2e56cf99c641521676339e20f
Chris Jefferson <chris@bubblescope.net>
2020-09-04 10:56:01 +0100

    Add cirrus gen script

---
+++ examples/run_latimes_cirrus.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+mkdir latimes-results
+
+for i in 3 2 1 0; do
+    for solver in  \
+            '{}' \
+            '{"baseSizeMUS": 100000}' \
+            '{"useUnsatCores": false}' \
+            '{"baseSizeMUS": 100000, "useUnsatCores": false}' \
+            '{"prechopMUSes12": true }' \
+            '{"prechopMUSes12": true, "useUnsatCores": false }' \
+            '{"prechopMUSes12": true, "useUnsatCores": false, "baseSizeMUS": 100000 }' \
+            '{"tryManyChopMUS": true}' \
+            '{"gallopingMUSes": true}' \
+            '{"gallopingMUSes": true, "baseSizeMUS": 100000}' \
+            '{"minPrecheckStepsMUS": true}' \
+            '{"minPrecheckMUS": true}' 
+    
+    do
+        echo 'cd demystify/examples; (time ./latimes.py '\'$i\'' '\'$solver\'') > '\'latimes-results/$i-$solver.html\'' 2> '\'latimes-results/$i-$solver.log\'''
+    done
+    wait
+done
