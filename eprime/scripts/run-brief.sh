7298848fac34358c8a5d6d956caf606e57ab6e4c
Chris Jefferson <caj21@st-andrews.ac.uk>
2021-01-06 13:22:26 +0000

    Limit steps

--- eprime/scripts/run-brief.sh
+++ eprime/scripts/run-brief.sh
@@ -1,1 +1,1 @@
-python3 ../demystify --eprime $1 --eprimeparam $2 --cores 4 --incomplete --repeats 20 > $2.out.html 2> $.err
+python3 ../demystify --eprime $1 --eprimeparam $2 --cores 4 --incomplete --repeats 20 --steps 4 > $2.out.html 2> $.err
