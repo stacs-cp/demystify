a04b3dad1591c8f73602a2ad03382da7e409ac4c
Chris Jefferson <caj21@st-andrews.ac.uk>
2021-01-07 16:47:34 +0000

    Run nodomains experiments

--- eprime/scripts/run-brief.sh
+++ eprime/scripts/run-brief.sh
@@ -1,1 +1,3 @@
-python3 ../demystify --eprime $1 --eprimeparam $2 --cores 4 --incomplete --repeats 20 --steps 4 > $2.out.html 2> $.err
+python3 ../demystify --eprime $1 --eprimeparam $2 --cores 4 --incomplete --repeats 20 --steps 10 > $2.out.html 2> $.err
+python3 ../demystify --eprime $1 --eprimeparam $2 --cores 4 --incomplete --repeats 20 --steps 10 --nodomains > $2.nodomains.html 2> $.nodomains.err
+
