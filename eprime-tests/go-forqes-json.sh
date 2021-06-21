acaf86c4cf7506ac931c483338c94475d5719744
Matthew McIlree <mjm42@st-andrews.ac.uk>
2021-06-21 16:24:19 +0100

    Change file names again for test failures.

--- eprime-tests/go-forqes-json.sh
+++ eprime-tests/go-forqes-json.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 filename=outputs/$(basename $1)-$(basename $2)
 
-python3 ../demystify --cores 1 --eprime ../eprime/$1 --eprimeparam ../eprime/$2 --forqes --json ${filename}.first-out > ${filename}.json.err 2>&1
-python3 -m json.tool ${filename}.first-out.json > ${filename}-forqes.json
-rm ${filename}.first-out.json
+python3 ../demystify --cores 1 --eprime ../eprime/$1 --eprimeparam ../eprime/$2 --forqes --json ${filename}-forqes.first-out > ${filename}-forqes.json.err 2>&1
+python3 -m json.tool ${filename}-forqes.first-out.json > ${filename}-forqes.json
+rm ${filename}-forqes.first-out.json
