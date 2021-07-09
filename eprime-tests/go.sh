4adc5be347431c6cfcb93a50b674a071a98aae8d
Chris Jefferson <caj21@st-andrews.ac.uk>
2021-07-09 12:25:01 +0100

    Update / cleanup tests

--- eprime-tests/go.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-filename=outputs/$(basename $1)-$(basename $2)
-python3 ../demystify --cores 1 --eprime ../eprime/$1 --eprimeparam ../eprime/$2 > ${filename}.out  2> ${filename}.err
