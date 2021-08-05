6a4a33e9940197122b143743178bb33ab9a35c8b
mmcilree <mjm42@st-andrews.ac.uk>
2021-08-05 12:24:04 +0100

    Updated Sudoku-Wiki tests with forced domains.

---
+++ demystify/musocus.py
@@ -0,0 +1,18 @@
+import math
+import logging
+import sys
+import math
+
+class OCUSMUSFinder:
+    def __init__(self, solver):
+        self._solver = solver
+        self._bestcache = {}
+
+        # Internals for OCUS
+        p_clauses = solver._cnf.clauses
+        p_ass = [[c] for c in solver._conlits]
+        p_weights = {c:20 for c in solver._conlits} # Demystify has no weighting so weight everything equally.
+        p_user_vars = solver._varsmt
+
+    def smallestMUS(self, puzlits):
+        #TO DO
